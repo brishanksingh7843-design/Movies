d89f5d158617588b21cfcb90906b01981fd5229c
Brishank <brishanksingh7843@gmail.com>
2026-07-24 21:24:30 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const cn = "movie-app-v1";
+const ftc = [
+  "./index.html",
+  "./main.css",
+  "./main.js",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./vinland-icon.jpg",
+  "./hajime-icon.jpg",
+  "./grave-icon.jpg",
+  "./death-icon.jpg",
+];
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cn).then((cache) => {
+      return cache.addAll(ftc);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
